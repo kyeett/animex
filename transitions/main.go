3acb1742b1f0e777df398692d8779147a1411ce9
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2019-04-28 23:09:10 +0200

    first step of 2d transitions

--- transitions/main.go
+++ transitions/main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"log"
 
-	"golang.org/x/image/colornames"
-
-	"github.com/peterhellberg/gfx"
-
 	shovelknightresources "github.com/kyeett/animex/resources/shovelknight"
 	"github.com/kyeett/ebitendrawutil"
 
@@ -24,52 +20,17 @@ var (
 )
 
 func update(screen *ebiten.Image) error {
-
 	// Draw the scenes
-	switch step {
-	case 0, 1, 2:
-		screen.DrawImage(scene1, &ebiten.DrawImageOptions{})
-	case 3, 4, 5:
-		screen.DrawImage(scene2, &ebiten.DrawImageOptions{})
-	}
+	screen.DrawImage(scene1, &ebiten.DrawImageOptions{})
 
-	// Draw the transition
-	scoreboardHeight := 44.0
-	maxRect := gfx.R(0, scoreboardHeight, screenWidth, screenHeight)
-	switch step {
-	case 0:
-		TransitionShrinkingRect(screen, maxRect, t, colornames.Black)
-	case 1:
-		// Increase faster during idle time
-		t += 0.01
-	case 2:
-		TransitionGrowingBorder(screen, maxRect, t, colornames.Black)
-	case 3:
-		// TransitionShrinkingRect(screen, maxRect, t, colornames.Black)
-		TransitionGrowingBorder(screen, maxRect, 1-t, colornames.Black)
-	case 4:
-		// Increase faster during idle time
-		t += 0.01
-	case 5:
-		TransitionGrowingBorder(screen, maxRect, t, colornames.Black)
-	}
-
-	t += 0.01
-	if t > 1 {
-		t = 0
-		step = (step + 1) % 6
-	}
-
+	// Draw scoreboard
 	screen.DrawImage(scoreboard, &ebiten.DrawImageOptions{})
 	return nil
 }
 
 func main() {
+	// Load resources
 	scene1Data, err := shovelknightresources.Asset("scene_1.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scene2Data, err := shovelknightresources.Asset("scene_2.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -77,11 +38,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	scene1 = ebitendrawutil.ImageFromBytes(scene1Data)
-	scene2 = ebitendrawutil.ImageFromBytes(scene2Data)
 	scoreboard = ebitendrawutil.ImageFromBytes(scoreboardData)
 
+	// Start the Ebiten update loop
 	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "my application"); err != nil {
 		log.Fatal(err)
 	}
